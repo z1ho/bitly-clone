c969168b14792867fb3495c4f7cef59e00ce5456
z1ho <ziho.89@gmail.com>
2016-04-18 17:07:57 +0800

    includes before_create

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -4,18 +4,21 @@ end
 
 post '/urls' do
 	long_url = params[:long_url]
-	my_url = Url.new(long_url: long_url)	#assigning a new instance
-	@short_url = my_url.shorten						#use the shorten method (ie .shorten) assign the shortened url to a variable
-	my_url.save														#save it
+	@link = Url.create(long_url: long_url)
+	# my_url = Url.new(long_url: long_url)	#assigning a new instance
+	# @short_url = my_url.shorten						#use the shorten method (ie .shorten) assign the shortened url to a variable
+	# my_url.save														#save it
 	erb :"static/index"
 end
 
 get '/:short_url' do
-	erb :"static/result"									#sends you to static/result.erb
+	erb :"static/result"										#sends you to static/result.erb
 	u = Url.find_by short_url: params[:short_url]
 	link = u.long_url 									
 	redirect link
 end
+
+
 
 ##PING'S NOTES
 #from site url to CONTROLLER
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+before_create :shorten
+	
 	def shorten
 		self.short_url = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
 	end
